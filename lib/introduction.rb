0f5df9ea9fdfcaf5e7a700b17a59d7cb2a323b08
John Glennan <glennan20@gmail.com>
2020-08-17 20:17:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("John", "Ruby")
+introduction("John")
+
+introduction_with_language("John Glennan", "Ruby")
